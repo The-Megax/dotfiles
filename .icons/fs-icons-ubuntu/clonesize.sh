8e8389c864283f59263f2681a79d2217f4433b20
Megax <megax@yeahunter.hu>
2014-10-17 21:33:48 +0200

    * 24x24-es képméretek előállítva. Nem a 22x22-es lesz linkelve.

---
+++ .icons/fs-icons-ubuntu/clonesize.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+old_size="32"
+new_size="24"
+
+main () {
+  dir_clone actions
+  dir_clone animations
+  dir_clone apps
+  dir_clone categories
+  dir_clone devices
+  dir_clone emblems
+  dir_clone mimetypes
+  dir_clone places
+  dir_clone status
+}
+
+dir_clone() {
+  dirname=$1
+  cd $dirname
+
+  if [ ! -e $new_size ]; then
+    mkdir $new_size
+  fi
+
+  cd $old_size
+
+  for f in $(find -iname "*.png")
+  do
+    png_size_clone $f
+  done
+
+  cd ../../
+
+  if [ ! -e "$new_size"x"$new_size" ]; then
+    mkdir "$new_size"x"$new_size"
+  fi
+
+  cd "$new_size"x"$new_size"
+  ln -s ../$dirname/$new_size $dirname
+  cd ../
+}
+
+png_size_clone () {
+  f=$1
+  echo "Copy png file $new_size x $new_size: $f"
+  filename="${f##*/}"
+
+  #Symlink fix. ???
+  #if [ -f $f ] && [ -L $f ]; then
+  #  cp $f ../$new_size/${filename}
+  #fi
+  
+  #if [ -f $f ] && [ ! -L $f ]; then
+  #  convert $f -scale "$new_size"x"$new_size"! ../$new_size/${filename}
+  #fi
+
+  if [ -f $f ]; then
+    convert $f -scale "$new_size"x"$new_size"! ../$new_size/${filename}
+  fi
+}
+
+main
